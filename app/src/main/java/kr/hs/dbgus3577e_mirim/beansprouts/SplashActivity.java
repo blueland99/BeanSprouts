dc7003c2d9bf5df78b5437387837c6dd8ced296e
YuHyeon-Kim <dbgus3577@gmail.com>
2016-10-24 19:38:31 +0900

    #1 Create SplashActivity

--- app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/SplashActivity.java
+++ app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/SplashActivity.java
@@ -4,14 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-
 public class SplashActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         try {
-            Thread.sleep(4000);
+            Thread.sleep(3000);
         } catch (InterruptedException e){
             e.printStackTrace();
         }
