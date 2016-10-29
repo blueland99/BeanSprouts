0bacff84e90eb5eedbc8f5c96b18303ad25663b0
YuHyeon-Kim <dbgus3577@gmail.com>
2016-10-29 19:17:49 +0900

    #3 Add 9 Step

--- app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/MainActivity.java
+++ app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.hs.dbgus3577e_mirim.beansprouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.game_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
