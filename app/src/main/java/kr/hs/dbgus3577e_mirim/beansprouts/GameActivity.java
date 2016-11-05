5270001fa4af98b502f4c225995174cbdc8d94cf
YuHyeon-Kim <dbgus3577@gmail.com>
2016-11-05 19:06:47 +0900

    #8 fileIO

--- app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/GameActivity.java
+++ app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/GameActivity.java
@@ -3,28 +3,81 @@ package kr.hs.dbgus3577e_mirim.beansprouts;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.*;
 
 public class GameActivity extends Activity {
     // ImageView 배열 생성
-    int level = 5;
-    ImageView step[] = new ImageView[level];
+    //  level은 올렸다 내려오기
+
+    String dirPath;
+    int levelMax = 9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+
+        // ------------------------------------------------------
+        // Create new Directory
+        dirPath = getFilesDir().getAbsolutePath() + "/beanSprouts";
+
+        File file = new File(dirPath);
+        if (!file.exists()) { // 디렉토리가 없으면
+            file.mkdirs();
+            Toast.makeText(this, "새로운 디렉토리", Toast.LENGTH_SHORT).show();
+        }
+        if (file.listFiles().length <= 0) {
+            // txt 파일 생성
+            String text = "1";
+            File savefile = new File(dirPath + "/BeanSproutsLevel.txt");
+            try {
+                FileWriter fos = new FileWriter(savefile);
+                fos.write(text);
+                fos.close();
+                Toast.makeText(this, "txt 파일 생성", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+            }
+        }
+
+
+        // ------------------------------------------------------
+        // 파일 읽어오기
+        String levelStr = "";
+        String name = "BeanSproutsLevel.txt";
+        String loadPath = dirPath + "/" + name;
+        Log.v(null, "fileName : " + name);
+        Toast.makeText(this, loadPath, Toast.LENGTH_SHORT).show();
+        try {
+            FileReader fr = new FileReader(loadPath);
+            BufferedReader br = new BufferedReader(fr);
+            String temp = "";
+            while ((temp = br.readLine()) != null) {
+                levelStr = levelStr + temp;
+            }
+            Toast.makeText(this, levelStr, Toast.LENGTH_SHORT).show();
+            Log.v(null, "" + levelStr);
+        } catch (Exception e) {
+            Toast.makeText(this, "안들어옴", Toast.LENGTH_SHORT).show();
+        }
+
+        int level = Integer.parseInt(levelStr);
+
+        ImageView step[] = new ImageView[level];
         for (int i = 0; i < level; i++) {
             step[i] = (ImageView) findViewById(R.id.step1 + i);
             step[i].setOnClickListener(imgVHandler);
         }
         for (int i = 0; i < level; i++) {
-            step[i].setImageResource(R.drawable.step1+i);
+            step[i].setImageResource(R.drawable.step1 + i);
         }
-
     }
 
     View.OnClickListener imgVHandler=new View.OnClickListener() {
--- app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/PlayActivity.java
+++ app/src/main/java/kr/hs/dbgus3577e_mirim/beansprouts/PlayActivity.java
@@ -2,28 +2,44 @@ package kr.hs.dbgus3577e_mirim.beansprouts;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.AbsoluteLayout;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class PlayActivity extends Activity {
+    int levelMax = 9;
     int level;
     int beanHead;
+    int score = 0;
 
     int move_X, move_Y;
     int width, height;
     final int beanX = 160;
     final int beanY = (int)(beanX * 2.5);
     int b = 10;
+    ArrayList<ImageView> img;
+    ArrayList<Integer> arrX, arrY;
+
+    TextView Tscore;
+    String dirPath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,8 +47,11 @@ public class PlayActivity extends Activity {
         Intent intent = getIntent();
         level = intent.getIntExtra("level", 0);
         setContentView(R.layout.activity_play);
+        dirPath = getFilesDir().getAbsolutePath() + "/beanSprouts";
 
-        beanHead = level * 5;
+        Tscore = (TextView)findViewById(R.id.tscore);
+        Tscore.setText("계산 결과 : "+score);
+        beanHead = level * 1;
 
         DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
 
@@ -40,34 +59,33 @@ public class PlayActivity extends Activity {
         height = dm.heightPixels - beanY;
 
         Random Ran_move = new Random();
-        move_X = Ran_move.nextInt(width) + 1;
-        move_Y = Ran_move.nextInt(height) + 1;
-
-        Random ranX1 = new Random();
-        Random ranY1 = new Random();
-        Random ranX2 = new Random();
-        Random ranY2 = new Random();
-        setContentView(R.layout.activity_play);
-
 
         FrameLayout F = (FrameLayout)findViewById(R.id.frameLayout);
         AbsoluteLayout A1 = new AbsoluteLayout(this);
 
-        ArrayList<ImageView> img = new ArrayList<ImageView>();
+        img = new ArrayList<ImageView>();
         // --------------------- FrameLayout, AbsoluteLayout ---------------------------------
 
+        arrX = new ArrayList<Integer>();
+        arrY = new ArrayList<Integer>();
+
+        for (int i=0; i<beanHead; i++) {
+            arrX.add(Ran_move.nextInt(width) + 1);
+            arrY.add(Ran_move.nextInt(width) + 1);
+        }
         for (int i=0; i<beanHead; i++) {
             ImageView num = new ImageView(this);
             num.setBackgroundResource(R.drawable.bean1);
-            A1.addView(num, new AbsoluteLayout.LayoutParams(beanX, beanY, ranX1.nextInt(move_X), ranY1.nextInt(move_Y)));  //(가로,세로, x좌표, y좌표)
-            if (ranX1.nextInt(move_X) < (width-beanX) && ranX1.nextInt(move_X) > (beanX/2)) {
+            A1.addView(num, new AbsoluteLayout.LayoutParams(beanX, beanY, arrX.get(i), arrY.get(i)));  //(가로,세로, x좌표, y좌표)
+            if (arrX.get(i) < (width-(beanX/2)) && arrX.get(i) > (beanX/2)) {
                 A1.removeView(num);
-                A1.addView(num, new AbsoluteLayout.LayoutParams(beanX, beanY, ranX1.nextInt(move_X), ranY1.nextInt(move_Y)));  //(가로,세로, x좌표, y좌표)
-                if (ranY1.nextInt(move_Y) < (height-beanY) && ranY1.nextInt(move_Y) > (beanY/2)) {
+                A1.addView(num, new AbsoluteLayout.LayoutParams(beanX, beanY, arrX.get(i), arrY.get(i)));  //(가로,세로, x좌표, y좌표)
+                if (arrY.get(i) < (height-beanY) && arrY.get(i) > (beanY/2)) {
                     A1.removeView(num);
-                    A1.addView(num, new AbsoluteLayout.LayoutParams(beanX, beanY, ranX1.nextInt(move_X), ranY1.nextInt(move_Y)));  //(가로,세로, x좌표, y좌표)
+                    A1.addView(num, new AbsoluteLayout.LayoutParams(beanX, beanY, arrX.get(i), arrY.get(i)));  //(가로,세로, x좌표, y좌표)
                 }
             }
+            num.setId(i);
             img.add(num);
         }
         F.addView(A1);
@@ -75,12 +93,45 @@ public class PlayActivity extends Activity {
         for (int i=0; i<beanHead; i++){
             img.get(i).setOnClickListener(imgVHandler);
         }
+
     }
 
     View.OnClickListener imgVHandler=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(PlayActivity.this, "클릭됨", Toast.LENGTH_SHORT).show();
+            int id = v.getId();
+            img.get(id).setVisibility(View.GONE);
+            score += 100;
+           // Toast.makeText(PlayActivity.this, v.getId()+", "+(score/10), Toast.LENGTH_SHORT).show();
+            if (score == beanHead*100) {
+                Toast.makeText(PlayActivity.this, "CLEAR", Toast.LENGTH_SHORT).show();
+
+                // img.size();
+                Tscore.setText("계산 결과 : "+score);
+/*                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+                if (level != levelMax) {
+                    level++;
+                }
+
+                GameActivity ga = new GameActivity();
+                String text = String.valueOf(level);
+                File savefile = new File(dirPath + "/BeanSproutsLevel.txt");
+                try {
+                    FileWriter fos = new FileWriter(savefile);
+                    fos.write(text);
+                    fos.close();
+                } catch (IOException e) {
+                    Toast.makeText(PlayActivity.this, "안들어옴", Toast.LENGTH_SHORT).show();
+                }
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+            //Toast.makeText(PlayActivity.this, "클릭됨, score : "+score, Toast.LENGTH_SHORT).show();
+
         }
     };
 }
